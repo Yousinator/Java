dac65049775e7b9da0e5ea3c6a2eb661e8015f51
Yousinator <y.omusabeh@gmail.com>
2023-04-12 16:42:44 +0300

    commit

---
+++ Labs/LabWeek5/src/App.java
@@ -0,0 +1,77 @@
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] myArray = new int[30];
+        String[] myStrings = { "Saud", "Ahmad", "Yousef", "Amro", "Omar" };
+        Random rand = new Random();
+
+        // ! Number Array
+
+        fill(myArray);
+        print(myArray);
+        System.out.println("Sotred:");
+        bubbleSort(myArray);
+        print(myArray);
+
+        System.out.println("\n================================================================\n");
+
+        // ! String Array
+
+        printString(myStrings);
+        System.out.println("Sotred:");
+        stringBubbleSort(myStrings);
+        printString(myStrings);
+
+    }
+
+    public static void bubbleSort(int myArray[]) {
+
+        for (int i = 0; i < myArray.length - 1; i++) {
+            for (int j = 0; j < myArray.length - i - 1; j++) {
+                if (myArray[j] > myArray[j + 1]) {
+                    int temp = myArray[j];
+                    myArray[j] = myArray[j + 1];
+                    myArray[j + 1] = temp;
+                }
+            }
+        }
+
+    }
+
+    public static void stringBubbleSort(String myStrings[]) {
+
+        for (int i = 0; i < myStrings.length - 1; i++) {
+            for (int j = 0; j < myStrings.length - i - 1; j++) {
+                if (myStrings[j].compareTo(myStrings[j + 1]) > 1) {
+                    String temp = myStrings[j];
+                    myStrings[j] = myStrings[j + 1];
+                    myStrings[j + 1] = temp;
+                }
+            }
+
+        }
+
+    }
+
+    public static void fill(int myArray[]) {
+        Random rand = new Random();
+        for (int i = 0; i < myArray.length - 1; i++) {
+
+            myArray[i] = rand.nextInt(97);
+        }
+
+    }
+
+    public static void print(int myArray[]) {
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.printf("[%d],", myArray[i]);
+        }
+    }
+
+    public static void printString(String myArray[]) {
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.printf("[%s],", myArray[i]);
+        }
+    }
+}
